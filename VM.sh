5c3b3af12edc47d80ccdf3197afeefc50d23438d
Leonidas-source <66934329+Leonidas-source@users.noreply.github.com>
2021-12-31 06:59:30 +0000

    Add files via upload

---
+++ VM.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+user_check() {
+  clear
+  [ `/usr/bin/whoami` != root ] && error
+}
+error() {
+  clear
+  echo "You must be root to execute this script"
+  exit
+}
+user_access() {
+  clear
+  echo "set name of your user (not root!!!)"
+  read answr
+  gpasswd -a $answr libvirt
+}
+user_check
+pacman -S virt-manager libvirt qemu edk2-ovmf ebtables dnsmasq
+systemctl enable libvirtd
+virsh net-autostart default
+user_access
+clear
+echo "Please reboot your system"
